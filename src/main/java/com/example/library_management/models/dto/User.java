45d1422e4cc2e81cdecb4c2dfc44bf7409335e82
Emily <emily.zy.lin@gmail.com>
2025-03-21 21:29:29 -0500

    create User Class

---
+++ src/main/java/com/example/library_management/models/dto/User.java
@@ -0,0 +1,14 @@
+public class User {
+    private String userId;
+    private String password; //hashed
+    private String username;
+    private String fullName;
+    private boolean status;
+
+    public boolean resetPassword(String newPassword) {
+        this.password = newPassword;
+        return true;
+    }
+}
+
+
